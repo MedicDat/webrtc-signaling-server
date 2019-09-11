5dec11154d405b67f9508c48cb452d78b6470196
Korbi <haberederkorbinian@googlemail.com>
2019-09-11 13:50:49 +0200

    better uptime format

--- src/Server.js
+++ src/Server.js
@@ -11,6 +11,18 @@ log.setLevel(log.levels.ERROR) //change this to DEBUG for verbose
 
 app.use(express.static(path.join(process.cwd(),"dist")));
 
+var toHHMMSS = function (secs) {
+    var sec_num = parseInt(secs, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours + ':' + minutes + ':' + seconds;
+}
+
 export default class CallHandler {
 
     constructor() {
@@ -90,7 +102,7 @@ export default class CallHandler {
     }
     
     getUptime = () => {
-        return moment().diff(this.start_time, 'seconds') + "s";
+        return toHHMMSS(moment().diff(this.start_time, 'seconds'));
     }
     
     getPeers = () => {
