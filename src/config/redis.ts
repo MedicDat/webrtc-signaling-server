3fac68eb74d2bfc383f7f6de4246d6c964ebea5c
liquidiert <haberederkorbinian@googlemail.com>
2020-07-22 16:26:28 +0200

    feature redis auth
      - added redis authentication

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,5 +1,7 @@
 import log from 'loglevel';
 import redis from "redis";
+import fs from "fs";
+import sha512 from "js-sha512";
 import { promisify } from "util";
 
 const client = redis.createClient();
@@ -8,6 +10,8 @@ const getAsync = promisify(client.get).bind(client);
 client.on("error", function(error) {
   log.error(error);
 });
+
+client.auth(sha512.sha512(fs.readFileSync("/etc/redis/redis_pass").toString()));
 
 export default async function getJWTInfos() {
   return {
